7cd905c0c5f2a43e99450729573fbbea754fa01e
Armand Grillet <ArmandGrillet@users.noreply.github.com>
2016-02-28 11:29:59 +0100

    Problem 1 is now multithreaded.

--- algo/01.go
+++ algo/01.go
@@ -1,26 +1,40 @@
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
 package algo
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func E1() {
 	max := 1000
+	multiples := [...]int{3, 5}
 
+	multiplesFound := []int{}
+	sums := make(chan int, len(multiples))
 	sum := 0
 
-	start := time.Now()
-	for i := 0; i < max; i += 3 {
-		sum += i
+	for _, multiple := range multiples {
+		go multiplesSum(multiple, max, multiplesFound, sums)
+		sum += <-sums
+		multiplesFound = append(multiplesFound, multiple)
 	}
-	for i := 0; i < max; i += 5 {
-		if i%3 != 0 {
-			sum += i
-		}
-	}
-	elapsed := time.Since(start)
 
 	fmt.Println(sum)
-	fmt.Println(elapsed)
+}
+
+func multiplesSum(quantity int, max int, avoid []int, ch chan int) {
+	sum := 0
+	for i := 0; i < max; i += quantity {
+		sum += i
+		for _, avoidQuantity := range avoid {
+			if i%avoidQuantity == 0 {
+				sum -= i
+				break
+			}
+		}
+	}
+	ch <- sum
 }
